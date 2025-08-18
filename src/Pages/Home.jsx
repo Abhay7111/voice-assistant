cc4b3ed150f92810a90b87b9d00a2e629aa38ff0
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-08-19 00:15:45 +0530

    fit to mobile screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import VoiceAssistant from '../Components/Voiceassistant'
 
 function Home() {
   return (
-    <div className='w-full h-screen flex items-center justify-center'><div className='w-full max-w-[700px] h-full'><VoiceAssistant/></div></div>
+    <div className='w-full h-[100dvh] flex items-center justify-center'><div className='w-full max-w-[700px] h-full'><VoiceAssistant/></div></div>
   )
 }
 
