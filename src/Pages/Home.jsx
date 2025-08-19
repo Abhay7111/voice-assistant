f0866ee301fa320305226d26f17bc33118400eae
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-08-19 21:20:55 +0530

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import VoiceAssistant from '../Components/Voiceassistant'
-import { ThemeToggle } from '../Components/Theam/Theam'
 
 function Home() {
   return (
     <div className='w-full h-[100dvh] flex items-center justify-center'>
-      <ThemeToggle />
       <div className='w-full max-w-[700px] h-full'>
         <VoiceAssistant/>
       </div>
